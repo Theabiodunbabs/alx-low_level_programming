fcf04f96c954a1db95d9fceaa7c95260eed78e51
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-13 09:48:38 -0700

    Multilingual

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	/* Texting without prints */
+	puts("Programing is like building a multilingual puzzle");
+	return (0);
+}

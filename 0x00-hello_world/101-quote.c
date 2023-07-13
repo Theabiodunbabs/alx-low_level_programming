0c51dd601e245747d82740788c2e299ea2b1cf89
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-13 13:21:34 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success0
+ */
+int main(void)
+{
+	/* A piece of art is useful */
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

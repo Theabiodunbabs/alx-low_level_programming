5b6200e84a795d7acd9f330c20daa2abacebb5f5
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-13 10:34:50 -0700

    trying again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main(void); /* Program should compile without warnings */
+int main(void); 
+	/* Program should compile without warnings */
 
 int main(void)
 {
 	/* Adding text with wall */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }

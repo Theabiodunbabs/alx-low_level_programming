995c73396f9d6368a23a693529349713a9bd0727
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-13 10:20:18 -0700

    adding texts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+int main ()
+{
+	/* My second project */
+
 int main(void)
 {
 	/* Adding text with wall */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
 }

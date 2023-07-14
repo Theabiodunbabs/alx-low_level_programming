ea496497208b0597a58d3553c5329ed3e5c6f7ce
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-14 08:28:38 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* this program prints alphabets */
+int main(void)
+{
+	/* set character for alphabets */
+
+	char m;
+
+	for (m = 'a'; m <= 'z'; m != 'e'; m != 'q'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+
+	return (0);
+}

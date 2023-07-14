80c31567e091d97253dbfefa7d00dd33fa209156
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-14 10:18:28 -0700

    ready finally

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
+ *
+ * Description: print two digits combination.
  *
  * Return: Always 0 (Success)
  */
-/* prints combination of two digits */
-
 int main(void)
 {
 	int i, j;
 
 	for (i = 0; i <= 98; i++)
 	{
-		for (j = i; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');

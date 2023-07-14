3f855205ddba1b661b432e234600858383b5accd
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-14 09:18:04 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+/*this program prints hexadecimal digits */
+int main(void)
+{
+	char digit;
+	char letter;
+
+	/* print digits */
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+
+	/* print letters */
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0)
+}

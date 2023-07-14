524d57c60554a1ace1312bab418c0dabfa5db91e
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-14 06:03:21 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* this program prints alphabets */
+int main (void)
+{
+	char m;
+	for (m = 'a'; c <= 'z'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+
+	return (0);
+}

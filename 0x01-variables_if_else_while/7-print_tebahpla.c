e51f808c88ad5ace945031117ed9f5618436c5ce
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-14 09:03:24 -0700

    ready 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* this program prints alphabets */
+int main(void)
+{
+	/* print alphabets in reverse */
+
+	char m;
+
+	for (m = 'z'; m >= 'a'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+
+	return (0);
+}

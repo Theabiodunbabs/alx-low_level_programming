080599e772e6c5428f1588b7ef9dbfce31f24ae2
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-14 10:32:07 -0700

    now

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	/* A piece of art is useful */
 	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+
 	write(2, message, sizeof(message) - 1);
-       	
+	
 	return (1);
 }

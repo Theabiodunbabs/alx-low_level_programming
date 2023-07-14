39573ccc5e177a379063f0b6d8159d1643e7a792
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-14 08:46:33 -0700

    resolving

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,11 +10,11 @@ int main(void)
 {
 /* print numbers in base 10 */
 
-	int num = 0;
+	int num;
 
-	for (num == '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		printf("%d\n");
+		printf("%d\n", num);
 	}
 
 	return (0);

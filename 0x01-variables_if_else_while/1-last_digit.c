e9f06a61055b1410abae21b01af53d8120be901c
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-14 05:50:16 -0700

    try nw

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,29 +10,28 @@
 /* This program shows the usage of if-else statements */
 int main(void)
 {
-        /* Function will set a value for n */
-        int n;
+	/* Function will set a value for n */
+	int n;
 	int lastdigit = 0;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
 	/* getting the last digit of n */
 	lastdigit = n % 10;
 
 	printf("Last digit of %d is %d and is ", n, lastdigit);
 
-        if (lastdigit > 5)
-        {
-        printf("greater than 5\n");
-        }
-        else if (n == 0)
-        {
-        printf("0\n");
-        }
-        else
-        {
-        printf("less than 6 and not 0\n");
-        }
-        return (0);
+	if (lastdigit > 5)
+	{
+	printf("greater than 5\n");
+	}
+	else if (lastdigit = 0)
+	{
+	printf("0\n");:	}
+	else
+	{
+	printf("less than 6 and not 0\n");
+	}
+	return (0);
 }

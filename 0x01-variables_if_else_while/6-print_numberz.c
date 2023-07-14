7299c8ec9e2b94072d74ccfc0177769d23856a82
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-14 08:57:15 -0700

    putchar for numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* this program prints alphabets */
+int main(void)
+{
+/* print numbers in base 10 */
+
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0'+ num);
+	}
+
+	putchar(\n');
+	return (0);
+}

0d80249ed3e7e0fc6ab549fbc861f23a872aa166
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-14 10:38:44 -0700

    i dey o

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -6,10 +5,12 @@
  *
  * Return: Always 0 (Success0
  */
+
 int main(void)
 {
-	/* A piece of art is useful */
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, sizeof(message) - 1);
-	return (1);
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    write(2, message, sizeof(message) - 1);
+
+    return 1;
 }

a4c3b84e6d4d57e9c4e0add4e9428778b3138e4a
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-18 12:52:31 -0700

    betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,15 @@
 
 /**
  * _islower - checks if a letter is lowercase
- * c is the case study
+ * @c: the case study
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
+	/* Check if c is int he range */
 	if (c >= 'a' && c <= 'z')
-		
+
 		return (1);
 	else
 		return (0);

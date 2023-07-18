8f8a907a18b342edb141fa42c6ced5fd1768a2eb
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-18 12:35:36 -0700

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints lowcase a to z 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int count;
+
+	for (count = 0; count < 10; count ++)
+	{
+		for (alpha = 'a'; alpha <= 'a'; alpha++)
+		{
+			_putchar(alpha);
+		}
+	}
+	_putchar('\n');
+}

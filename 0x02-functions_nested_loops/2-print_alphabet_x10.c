ad57c150d65d955be0a28fb1d006ebf3a9978595
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-18 12:37:49 -0700

    10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 	char alpha;
 	int count;
 
-	for (count = 0; count < 10; count ++)
+	for (count = 0; count < 10; count++)
 	{
-		for (alpha = 'a'; alpha <= 'a'; alpha++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
 		}

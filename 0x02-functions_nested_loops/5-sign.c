228f38be74c22ee0cc5f36a7849b9ced79893ca0
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-18 13:26:02 -0700

    ready

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sing - Prints the sign of a number
+ * n: The number to check and is used as case study
+ *
+ * Return: 1 if n is > 0, 0 if n = 0, -1 if n < 0
+ */
+int print_sing(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

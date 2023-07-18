01d9fb14f1a0d3def443a81643a2ea7d63dac645
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-18 13:20:34 -0700

    ready

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,11 +10,11 @@ int _isalpha(int c)
 {
 	/* Check if c is an uppercase or lowercase letter */
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-			{
-			return (1); /* c is an alphabet */
-			}
-			else
-			{
-			return (0); /* c is not alphabetic */
-			}
+	{
+		return (1); /* c is an alphabet */
+	}
+	else
+	{
+		return (0); /* c is not alphabetic */
+	}
 }

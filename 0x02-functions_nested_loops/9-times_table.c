e0d20f3064e1b2008853d73a8df5169b7a159f90
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-18 14:08:14 -0700

    product

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+	int a, b, product;
+	
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			product = a * b;
+			if (b == 0)
+				_putchar(product + '0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10)
+					_putchar(' ');
+				_putchar(result + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}

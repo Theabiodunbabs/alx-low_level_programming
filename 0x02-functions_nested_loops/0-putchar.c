f1e2013bd7eb8ddb2f18909067d49657e504ee7b
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-18 11:34:43 -0700

    getting started

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* program to print _putchar */
+	printf("_putchar\n");
+	return (0);
+}

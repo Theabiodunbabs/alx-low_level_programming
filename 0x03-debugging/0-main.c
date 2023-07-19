7bc1e376923ef4585364f73c4a5c53794eed6d91
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-19 08:48:32 -0700

    task o

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests the positive_or_negative() function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	i = 0; /* assign the value 0 to i */
+	
+	positive_or_negative(i); /* call the positive_or_negative() function */
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype */
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */

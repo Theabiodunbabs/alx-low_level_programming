03366c0d5057073d537b7286315eb37b35ec04f7
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-19 06:02:36 -0700

    asdfsr

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,8 @@ void print_times_table(int n)
 	if (n < 0 || n > 15)
 		return; /* If n is outside the valid range, do not print */
 
-	int i, j;
+	int i;
+	int j;
 	int product;
 
 	for (i = 0; i <= n; i++)

97ea42bc95e32779681ce644a49ff5ff4bddc888
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-20 12:50:19 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers from 0 to 14, repeated 10 times)
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 0)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+
+		_putchar('\n');
+	}
+}

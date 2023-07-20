b76602162cb677ecc3eeb081841c6945dbd4ed44
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-20 11:25:03 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9, followed by a new line
+ */
+void print_numbers(void)
+{
+	char a;
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}

8ff7f80b2180501633a0b4cacb391da7ea57684b
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-21 04:06:23 -0700

    task 1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,8 @@
 /* Function prototype */
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+void convert_day(int month, int day);
 
 #endif /* MAIN_H */

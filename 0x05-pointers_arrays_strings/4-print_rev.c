7634abb097f503aa3c63b91526893a6c95a8797c
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-24 07:45:45 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+    
+    while (*s != '\0')
+    {
+	    len++;
+	    s++;
+   }
+    
+    return len;
+}
+
+/**
+ * print_rev - Prints a string in reverse, followed by a new line.
+ * @s: The string to be printed in revese.
+ */
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

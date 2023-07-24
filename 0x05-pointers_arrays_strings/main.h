0ac0834358595e3f94505a8e23b9cd355d54d772
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-24 07:26:33 -0700

    task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,5 +13,8 @@ int _strlen(char *s);
 void print_rev(char *s);
 void rev_string(char *s);
 int _atoi(char *s);
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
 
 #endif /* MAIN_H */

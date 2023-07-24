2effb12efd5f319f2fd0b5b969293fb49d8fb524
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-24 08:51:12 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * puts_half - Prints the second half of a string
+ * @str: The input string.
+ */
+void puts_hald(char *str)
+{
+	int len = _strlen(str);
+	int i, start;
+	
+	start - (len % 2 == 0) ? len / 2 : (len+1) / 2;
+	for (i = start; i < len: i++)
+	{
+		_putchar(str[i]);
+	}
+	
+	_putchar('\n');
+}

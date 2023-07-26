1e4d499baaddf3c518f3d64ce7a5427952f55a0e
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-07-26 09:43:08 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string for the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return len;
+}
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = _strlen(dest);
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[dest_len +i] = src[i];
+	}
+
+	dest[dest_len + i] = '\0';
+
+	return dest;
+}

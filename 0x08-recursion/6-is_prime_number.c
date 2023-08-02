85c46a416e9939697f85e272d8bcd9a92d756e2c
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-08-02 10:20:40 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,17 +6,30 @@
  *
  * Return: 1 if the number is a prime number, 0 otherwise.
  */
+int is_prime_helper(int n, int i);
+
 int is_prime_number(int n)
 {
-	int i;
 	if (n <= 1)
 		return (0);
 
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
+	return is_prime_helper(n, 2);
+}
 
-	return (1);
+/**
+ * is_prime_helper - Recursive helper function to check for prime numbers.
+ * @n: The number to be checked.
+ * @i: The divisor to check for factors.
+ *
+ * Return: 1 if the number is a prime number, 0 otherwise.
+ */
+int is_prime_helper(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+
+	if (n % i == 0)
+		return (0);
+
+	return (is_prime_helper(n, i + 1));
 }

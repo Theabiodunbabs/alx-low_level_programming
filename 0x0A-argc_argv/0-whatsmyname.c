1d7486097aa21449bb5ad4b2854761f951b218f5
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-08-04 05:32:14 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * This program prints its name
+ * @argc: arguement count
+ * @argv: arugement vector
+ *
+ * Return: Always 0(sucess)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+	printf("%s\n", argv[0]);
+	}
+	else
+	{
+		fprintf(stderr, "Error:No program name provided.\n");
+		return (1);
+	}
+	return (0);
+}

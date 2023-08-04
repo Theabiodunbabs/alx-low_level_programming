c3ef414c9b73e962383f1c559e1ef7885dc54ed8
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-08-04 04:18:20 -0700

    task 0

---
+++ 0x09-static_libraries/_strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	while (*s != '\0')
+	{
+		char *a = accept;
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				count++;
+				break;
+			}
+			a++;
+		}
+		if (*a == '\0')
+			break;
+		s++;
+	}
+	return (count);
+}

1562b52ae9ffb74adc2a2dc10d09f30a318b3909
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-08-07 11:27:02 -0700

    task 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,32 +11,29 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *result;
-	int total_length;
+	int total_length = 0;
 	int i, j, k = 0;
+	char *result;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-	{
 		total_length += strlen(av[i]);
-	}
 
 	total_length += ac - 1;
 	result = malloc((total_length + 1) * sizeof(char));
 
 	if (result == NULL)
-		return (NULL);
-
+		return NULL;
+	
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j]; j++)
-		{
 			result[k++] = av[i][j];
-		}
-		
-		result[k++] = '\n';
+
+		if (i != ac - 1)
+			result[k++] = '\n';
 	}
 
 	result[k] = '\0';

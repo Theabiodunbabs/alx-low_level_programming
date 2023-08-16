c334570d2c75cb3b66db060bcab1cdaa0710eb1c
Theabiodunbabs <abiodunbabs8@gmail.com>
2023-08-16 10:28:40 -0700

    task 3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,26 +1,34 @@
-#include <stdio.h>
+#include <stddef.h>
 #include "function_pointers.h"
 
-int is_even(int num)
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	return (num % 2 == 0);
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
 }
 
-int main(void)
+int is_98(int elem)
 {
-	int array[] = {1, 3, 5, 8, 10, 15};
-	int size = sizeof(array) / sizeof(array[0]);
+	return (98 == elem);
+}
 
-	int index = int_index(array, size, is_even);
+int is_strictly_positive(int elem)
+{
+	return (elem > 0);
+}
 
-	if (index != -1)
-	{
-		printf("First even number is at index %d\n", index);
-	}
-	else
-	{
-		printf("No even number found\n");
-	}
-
-	return (0);
+int abs_is_98(int elem)
+{
+	return (elem == 98 || -elem == 98);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int is_98(int elem);
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
+int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* MAIN_H */
+#endif /* FUNCTION_POINTERS_H */
